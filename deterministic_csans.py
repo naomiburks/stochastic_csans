ab68ec1c12c6d65258cc0867c8f9b53bf56abf98
Naomi Burks <burks@umn.edu>
2024-10-16 08:35:12 -0500

    progress

--- deterministic_csans.py
+++ deterministic_csans.py
@@ -1,6 +1,9 @@
 from model import Model
 from itertools import product
 from scipy.special import binom
+from scipy.integrate import odeint
+from constants import ZEROS
+from copy import copy
 
 class CSANDetModel(Model):
     
@@ -23,6 +26,21 @@ class CSANDetModel(Model):
     
     def _get_edt_index(self, e_bound, t_bound):
         return self.e_receptors + self.t_receptors + 3 + self.e_receptors * self.t_receptors + e_bound * self.t_receptors + t_bound
+
+    def d(self, state):
+        return state[self._get_d_index()]
+    
+    def t(self, state):
+        total = 0
+        for i in range(self.t_receptors + 1):
+            total += state[self._get_t_index(i)]
+        return total 
+    
+    def e(self, state):
+        total = 0
+        for i in range(self.e_receptors + 1):
+            total += state[self._get_e_index(i)]
+        return total
 
     def get_derivative(self, state, parameters):
         derivative = [0] * self.dimension
@@ -70,24 +88,34 @@ class CSANDetModel(Model):
             for j in range(self.t_receptors):
                 index = self._get_et_index(i, j)
                 e_change += state[index] * parameters["d_ET"]
-                if j == 0:
+                if i == 0:
                     continue
-                index = self._get_edt_index(i, j - 1)
-                e_change += state[index] * parameters["d_ET"]
+                index = self._get_edt_index(i - 1, j)
+                e_change += state[index] * parameters["d_EDT"]
 
             # incoming csan bindings
-            
-
+            if i != 0:
+                index = self._get_e_index(i - 1)
+                e_change += state[index] * (self.e_receptors - i + 1) * parameters["lambda_E"]
+                
 
             # outgoing csan bindings
+            if i != self.e_receptors:
+                index = self._get_e_index(i)
+                e_change += state[index] * (self.e_receptors - i) * parameters["lambda_E"]
 
             # incoming csan absorbtions
-
+            pass
             # outgoing csan absorbtions
-
+            pass
             # dimer/trimer formation
             pass
-        
+            
+            # put change in derivative
+            index = self._get_e_index(i)
+            derivative[index] = e_change
+
+
         # get Ts derivatives
         for i in range(self.t_receptors + 1):
             # births
@@ -104,3 +132,25 @@ class CSANDetModel(Model):
         # get EDTs derivatives 
         for i, j in product(range(self.e_receptors), range(self.t_receptors)):
             pass
+
+        return derivative
+    
+    def get_initial_state(self, d, e, t):
+        state = [0] * (self.e_receptors + self.t_receptors + 2 * self.e_receptors * self.t_receptors + 3)
+        state[self._get_d_index()] = d
+        state[self._get_e_index(0)] = e
+        state[self._get_t_index(0)] = t
+        return state
+
+    def run(self, parameters, initial_state, timepoints):
+        f = lambda x, t: self.get_derivative(x, parameters)
+        result = odeint(f, initial_state, timepoints)
+        return result
+
+if __name__ == "__main__":
+    model = CSANDetModel(5, 5)
+    initial_state = model.get_initial_state(10, 10, 10)
+    parameters = copy(ZEROS)
+    parameters["b_E"] = 1
+    res = model.run(parameters, initial_state, [i / 10 for i in range(30)])
+    print(res)
